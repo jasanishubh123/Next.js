329a274675ef0f2cebca5c8404bda5cfb09807e7
jasanishubh123 <jasanishubham@gmail.com>
2020-07-12 21:29:40 +0530

    Some Change

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div className="container">
-    <a className="navbar-brand" href="#">Start Bootstrap</a>
+    <a className="navbar-brand" href="#">Movie Detail</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
